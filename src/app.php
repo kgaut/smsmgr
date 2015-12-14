87a674ee08c223cc9dc25f9592b7ec32f3fcab71
Kevin Gautreau <contact@kgaut.net>
2015-12-14 10:15:17 +0100

    scaffolding app

--- src/app.php
+++ src/app.php
@@ -7,29 +7,8 @@ use Kgaut\BBF\NewsletterSubscriptionForm;
 $app = require(__DIR__.'/bootstrap.php');
 
 $app->get('/', function () use ($app)  {
-  $lang = $app['session']->get('current_language');
-  return $app->redirect('/'.$lang);
+  return "laaa";
 });
 
-$app->match('/{lang}', function($lang,Request $request) use($app) {
-  if (is_file(__DIR__ . '/locales/' . $lang.'.yml')) {
-    $app['session']->set('current_language', $lang);
-    $app['translator']->setLocale($lang);
-  }
-  else {
-    $app->redirect('/');
-    exit;
-  }
-  $form = NewsletterSubscriptionForm::getForm($app);
-  $data = NewsletterSubscriptionForm::handleRequest($form,$app,$request);
-
-  if(!$data) {
-    return $app['twig']->render('page-home.twig', array('form'=>$form->createView()));
-  }
-  else {
-    return $app['twig']->render('page-thanks.twig', array('data'=>$data));
-  }
-})->bind('front');
-;
 
 return $app;
