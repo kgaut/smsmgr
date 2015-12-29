f8c817d7f48670a05ac3c946f94167a63bf24f47
Kevin Gautreau <contact@kgaut.net>
2015-12-29 08:53:52 +0100

    Switch to silex-skeleton

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 define('BASEPATH',__DIR__);
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once BASEPATH.'/vendor/autoload.php';
 
-$app = require __DIR__.'/src/app.php';
+$app = require BASEPATH.'/src/app.php';
+require BASEPATH.'/config/prod.php';
+require BASEPATH.'/src/controllers.php';
+
+$app->run();
+
 
 $app->run();
--- src/app.php
+++ src/app.php
@@ -1,20 +1,36 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints;
-use Kgaut\BBF\NewsletterSubscriptionForm;
+
+define("ROOT", __DIR__ . "/../");
+define("ROOT_SRC", __DIR__ );
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+$app = new Application();
+$app->register(new UrlGeneratorServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+
+$conf_file = ROOT.'/config/'.str_replace('www.','',$_SERVER['HTTP_HOST']).'.php';
+if(is_file($conf_file)) {
+  require($conf_file);
+}
+else {
+  require(ROOT_SRC.'/config/prod.php');
+}
 
 
-$app = require(__DIR__.'/bootstrap.php');
 
-$app->get('/', function () use ($app)  {
-  $streamer = Prewk\XmlStringStreamer::createStringWalkerParser(BASEPATH.$file);
-  $i = 0;
-  while ($node = $streamer->getNode() && $i<=50) {
-    $i++;
-    $simpleXmlNode = simplexml_load_string($node);
-    dump($simpleXmlNode);
-  }
-});
+$app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
 
+    return $twig;
+}));
 
 return $app;
